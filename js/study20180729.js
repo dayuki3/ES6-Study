c666eea4a5421fb75ed6a31d07a0d058fe94d5f3
dayuki <dayuki3@naver.com>
2018-08-19 19:33:06 +0900

    지난번 작업 분량분리

--- js/study20180729.js
+++ js/study20180729.js
@@ -1,13 +1,13 @@
 function changeNumber(state) {
     const rangeMAX = 50;
     const rangeMIN = -50;
-    const numberInput = document.querySelector(".input-number-area");
+    const numberInput = document.querySelector(".input-result-area");
     let numberInputValue = Number(numberInput.value);
     let newValue; 
 
-    if (state == "increase" && numberInputValue < 50){
+    if (state == "increase" && numberInputValue < rangeMAX){
         newValue = numberInputValue + 1;
-    } else if(state == "decrease" && numberInputValue > -50) {
+    } else if(state == "decrease" && numberInputValue > rangeMIN) {
         newValue = numberInputValue - 1;
     } else {
         return;
@@ -16,37 +16,13 @@ function changeNumber(state) {
     numberInput.value = newValue;
 }
 
-class changeNumber2 {
-    constructor (config) {
-        this.config = config;
-        this.render(config.id);
-    }
-
-    render(id){
-        const wrapper = document.getElementById(id);
-        wrapper.innerHTML = `
-            <input type="text" onChange= { this.blabal } value="${this.config.defaultValue}" class="input-number-area">
-            <button type="button" class="number-button number-button-increase">증가</button>
-            <button type="button" class="number-button number-button-decrease">감소</button>
-        `;
-    }
-}
-
-
-
-
 document.onreadystatechange = function () {
     if (document.readyState == "interactive") {
-        let buttonIncrease = document.querySelector(".number-button-increase");
-        let buttonDecrease = document.querySelector(".number-button-decrease");
+        let buttonIncrease = document.querySelector(".button-number-increase");
+        let buttonDecrease = document.querySelector(".button-number-decrease");
 
         buttonDecrease.addEventListener('click',function(){changeNumber('decrease')});
         buttonIncrease.addEventListener('click',function(){changeNumber('increase')});
-
-        /* 20180806 */
-        const test = new changeNumber2(1,2,3);
-        test.text("a", "b");
-        var spiner = new changeNumber2({id: "spiner"});
     }
 }
 
