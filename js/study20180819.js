54fc50b407f2baac78ffee81efe16fb2974fc7b6
dayuki <dayuki3@naver.com>
2018-09-01 19:39:03 +0900

    1. 기본 값이 없을때에 대한 대처 2. min, max 값이 없을 때 infinite 3. constructor에서 render, addEvent하도록 수정 4. 버튼, 입력 부분 각 함수에서 따로 접근하지 않도록 개선

--- js/study20180819.js
+++ js/study20180819.js
@@ -1,71 +1,82 @@
 
-class Spinner{
-    constructor (config) {
+class Spinner {
+    constructor(config) {
         this.id = config.id;
-        this.defaultValue = config.defaultValue;
-        this.step = config.step;
-        this.base = config.base;
-        this.min = config.min;
-        this.max = config.max;
-    }
+        this.defaultValue = (config.defaultValue) ? config.defaultValue : '';
+        this.step = (config.step) ? config.step : 1;
+        this.base = (config.base) ? config.base : 0;
 
-    addEvent(){
-        //수정 필요 id받아오잖아!
+        if (config.min) {
+            this.min = config.min;
+        }
+        if (config.max) {
+            this.max = config.max;
+        }
+
+        this.render();
+
         this.buttonIncrease = document.querySelector(".button-number-increase");
         this.buttonDecrease = document.querySelector(".button-number-decrease");
+        this.resultArea = document.querySelector(".input-result-area");
 
+        this.addEvent();
+    }
+
+    addEvent() {
         this.buttonIncrease.addEventListener('click', this.increase.bind(this));
         this.buttonDecrease.addEventListener('click', this.decrease.bind(this));
     }
 
-    getBaseNumber(resultArea){
+    getBaseNumber(resultArea) {
         const resultAreaValue = Number(resultArea.value);
-        const currentValue = (Number.isNaN(resultAreaValue))?  0 : resultAreaValue ;
+        const currentValue = (Number.isNaN(resultAreaValue)) ? this.base : resultAreaValue;
 
         return currentValue;
     }
 
-    isValid(value) {
-        //min, max 이내에 있는지 체크해서 true false 반환
-        if(value > this.max || value < this.min){
-            return false;
-        }
+    isValidIncreasedValue(value) {
+        //max값이 있으면 max이상 넘어갈 수 없다. max가 없으면 infinite 
+        if (!this.max) return true;
 
-        return true;
+        if (value <= this.max) { 
+            return true;
+        }
     }
 
-    //클래스 내 멤버함수를 arrow function으로 선언하게되면 
-    //해당 함수에서 this 를 사용하기위해서 따로 bind를 하지 않아도 되지만
-    //arrow function으로 멤버함수를 선언하려면 기본 es6로는 안되고 babel등의 설치가 필요하다.
-    //ES7 (es2017) 에 들어가는건가?
-    //increase = () => {
-    increase(){
-        let resultArea = document.querySelector(".input-result-area");
-        let currentValue = this.getBaseNumber(resultArea);
+    isValidDecreasedValue(value) {
+        //min값이 있으면 min이하로 내려갈 수 없다. min이 없으면 infinite 
+        if (!this.min) return true;
+
+        if (value >= this.min) {
+            return true;
+        }
+    }
+
+    increase() {
+        let currentValue = this.getBaseNumber(this.resultArea);
         let result = currentValue + this.step;
-    
-        if (!this.isValid(result)){
-            return ;
+
+        if (!this.isValidIncreasedValue(result)) {
+            return;
         }
 
-        resultArea.value = result;
+        this.resultArea.value = result;
     }
 
     decrease() {
-        let resultArea = document.querySelector(".input-result-area"); //dom에 접근하는 비용이 크니까 한번만 가져와서 재사용하도록하자
-        let currentValue = this.getBaseNumber(resultArea);
+        let currentValue = this.getBaseNumber(this.resultArea);
         let result = currentValue - this.step;
-    
-        if (!this.isValid(result)){
-            return false; 
+
+        if (!this.isValidDecreasedValue(result)) {
+            return false;
         }
 
-        resultArea.value = result;
+        this.resultArea.value = result;
     }
 
-    render(){
+    render() {
         const wrapper = document.getElementById(this.id);
-        
+
         wrapper.innerHTML = `
             <input type="text" placeholder="${this.defaultValue}" class="input-result-area">
             <button type="button" class="button-number button-number-increase">증가</button>
@@ -79,18 +90,14 @@ document.onreadystatechange = function () {
 
         const config = {
             id: "spinner",
-            defaultValue: "숫자를 증감시킵니다.",    //없으면 undefined
-            step: 5,                            //없으면 NaN
+            defaultValue: "숫자를 증감시킵니다.",
+            step: 5,
             base: 0,
-            min: -20,                           //없으면 NaN
-            max: 20                             //없으면 NaN
+            min: -20,
+            max: 20
         }
 
         const spinner = new Spinner(config);
-
-        //constructor 내에 포함 가능함. 실행 시점을 조작하고싶다면 이렇게 하는 경우도 있음.
-        spinner.render();
-        spinner.addEvent();
     }
 }
 
