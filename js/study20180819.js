55ecd56fe709c9d5d13ffb4b49f5cce129d86e27
dayuki <dayuki3@naver.com>
2018-10-27 15:10:59 +0900

    Spinner 완성 엉ㅇ엉엉

--- js/study20180819.js
+++ js/study20180819.js
@@ -14,73 +14,84 @@ class Spinner {
         }
 
         this.render();
+        this._wrapper = document.getElementById(this.id)
 
-        this.buttonIncrease = document.querySelector(".button-number-increase");
-        this.buttonDecrease = document.querySelector(".button-number-decrease");
-        this.resultArea = document.querySelector(".input-result-area");
+        this.buttonIncrease = this._wrapper.querySelector(".button-number-increase");
+        this.buttonDecrease = this._wrapper.querySelector(".button-number-decrease");
+        this.resultArea = this._wrapper.querySelector(".input-result-area");
+        this.buttonDestroy = this._wrapper.querySelector(".button-destroy");
 
-        this.addEvent();
-        this.checkKeyInput();
+        this._increaseHandler = this.increase.bind(this);
+        this._decreaseHandler = this.decrease.bind(this);
+        this._arrowKeyHandler = this.arrowKeyEvent.bind(this);
+
+        this._addEvent();
+
+        this._currentValue = this.getBaseNumber(this.resultArea);
+
     }
 
-    addEvent() {
-        this.buttonIncrease.addEventListener('click', this.increase.bind(this));
-        this.buttonDecrease.addEventListener('click', this.decrease.bind(this));
+    _addEvent() {
+        this.buttonIncrease.addEventListener('click', this._increaseHandler);
+        this.buttonDecrease.addEventListener('click', this._decreaseHandler);
+        this.resultArea.addEventListener('keyup', this._arrowKeyHandler);
+        this.buttonDestroy.addEventListener('click', this.destroy.bind(this));
+
     }
 
-    checkKeyInput() {
-        let self = this;
-        document.onkeyup = function (e) {
-            self.arrowKeyEvent(e);
-            self.remainNumeric();
-        }
+    destroy() {
+        this.buttonIncrease.removeEventListener('click',this._increaseHandler);
+        this.buttonDecrease.removeEventListener('click',this._decreaseHandler);
+        this.resultArea.removeEventListener('keyup',this._arrowKeyHandler);
     }
+
     arrowKeyEvent(e) {
-        let self = this;
         switch (e.keyCode) {
             case 38://up
-                self.increase();
+                this.increase();
                 break;
             case 40://down
-                self.decrease();
+                this.decrease();
                 break;
         }
+
+        this.remainNumeric();
     }
 
-    remainNumeric() { 
+    remainNumeric() {
         let result = this.resultArea.value.replace(/[^-0-9]/g, '');
-        this.updateInputValue(result);
+        this.updateInputValue(Number(result));
     }
 
     getBaseNumber(resultArea) {
-        const resultAreaValue = Number(resultArea.value);
-        const currentValue = (Number.isNaN(resultAreaValue)) ? this.base : resultAreaValue;
+        const resultAreaValue = resultArea.value; 
+        const currentValue = (resultAreaValue == "") ? this.base : resultAreaValue;
 
-        return currentValue;
+        return Number(currentValue);
     }
 
     updateInputValue(result) {
-        this.resultArea.value = result;
+        this._currentValue = result;
+        this.resultArea.value = this._currentValue;
     }
     increase() {
-        let currentValue = this.getBaseNumber(this.resultArea);
-        let result = currentValue + this.step;
+        let result = this._currentValue + this.step;
 
         result = this.returnStepedResult(result);
 
         if (this.max) {
             //currentValue가 max보다 크면 동작하지않는다
-            if (currentValue >= this.max) {
+            if (this._currentValue >= this.max) {
                 return false;
             }
 
             //currentValue + step이 max를 넘어가면 max값을 반영한다.
-            if (currentValue < this.max && result > this.max) {
+            if (this._currentValue < this.max && result > this.max) {
                 result = this.max;
             }
 
             //최소값보다 작은 value일 때 증가 버튼을 누르면 최소값을 반환한다.
-            if (currentValue < this.min && this.min) {
+            if (this._currentValue < this.min && this.min) {
                 result = this.min;
             }
 
@@ -90,24 +101,23 @@ class Spinner {
     }
 
     decrease() {
-        let currentValue = this.getBaseNumber(this.resultArea);
-        let result = currentValue - this.step;
+        let result = this._currentValue - this.step;
 
         result = this.returnStepedResult(result);
 
         if (this.min) {
             //currentValue가 min보다 작으면 동작하지않는다
-            if (currentValue <= this.min) {
+            if (this._currentValue <= this.min) {
                 return false;
             }
 
             //currentValue - step이 min보다 작아지면 min값을 반영한다.
-            if (currentValue > this.min && result < this.min) {
+            if (this._currentValue > this.min && result < this.min) {
                 result = this.min;
             }
 
             //최대값보다 큰 value일 때 감소 버튼을 누르면 최대값을 반환한다.
-            if (currentValue > this.max && this.max) {
+            if (this._currentValue > this.max && this.max) {
                 result = this.max;
             }
         }
@@ -126,7 +136,6 @@ class Spinner {
         return result
     }
 
-
     render() {
         const wrapper = document.getElementById(this.id);
 
@@ -134,6 +143,7 @@ class Spinner {
             <input type="text" placeholder="${this.defaultValue}" class="input-result-area">
             <button type="button" class="button-number button-number-increase">증가</button>
             <button type="button" class="button-number button-number-decrease">감소</button>
+            <div><button type="button" class="button-destroy">DESTROY</button></div>
         `;
     }
 }
@@ -145,12 +155,19 @@ document.onreadystatechange = function () {
             id: "spinner",
             defaultValue: "숫자를 증감시킵니다.",
             step: 5,
-            base: 0,
+            base: 10,
             min: -100,
             max: 100
         }
 
         const spinner = new Spinner(config);
+
+        const spinner2 = new Spinner({
+            id: "spinner1",
+            defaultValue: "전설 바보",
+            step: 5,
+            base: -10
+        })
     }
 }
 
